293cd4e384ffed676d17579ca4273879f373a4cb
Helio Junior <93434324+HelioDevv@users.noreply.github.com>
2024-02-23 19:35:56 -0300

    Four Commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,25 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 400.00));
+		list.add(new Product("Mouse", 80.00));
 		
 		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		list.removeIf(new ProductPredicate());
 		
 		for(Product p : list) {
 			System.out.println(p);
